6e7135312f7b31810be35c80898abaa67319b46a
CT1m3 <simas.banevicius26@gmail.com>
2024-06-05 12:11:35 +0300

    initial commit

---
+++ src/main/java/com/spring/BookRezervations/BookRezervationsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.BookRezervations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRezervationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRezervationsApplication.class, args);
+	}
+
+}
