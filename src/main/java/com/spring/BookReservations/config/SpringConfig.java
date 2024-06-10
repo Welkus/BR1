9932c045a336932afb079714d6365e8d6b8ff0a6
CT1m3 <simas.banevicius26@gmail.com>
2024-06-10 17:02:07 +0300

    Working login and register

--- src/main/java/com/spring/BookReservations/config/SpringConfig.java
+++ src/main/java/com/spring/BookReservations/config/SpringConfig.java
@@ -1,20 +1,26 @@
 package com.spring.BookReservations.config;
 
 import com.spring.BookReservations.repository.UserRepository;
+import com.spring.BookReservations.service.UserDetailsServiceImpl;
 import com.spring.BookReservations.service.UserService;
 import com.spring.BookReservations.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SpringConfig {
-    private final UserRepository userRepository;
 
     @Autowired
     public SpringConfig(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    }
+
+    @Bean
+    @Qualifier("UserDetailsService")
+    public UserDetailsService getuserDetailsService() {
+        return new UserDetailsServiceImpl();
     }
 
     @Bean
--- src/main/java/com/spring/BookReservations/repository/BookRepository.java
+++ src/main/java/com/spring/BookReservations/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.spring.BookReservations.repository;
 
 import com.spring.BookReservations.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
 }
